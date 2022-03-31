be5c9fb14fe28318599f47c1e29897a3015b73af
Azam <azam.inoyatov.1998@gmail.com>
2022-04-01 01:47:46 +0500

    created index view ))

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Congratulations!</h1>
 
-        <p class="lead">You have successfully logged in your Library application.</p>
+        <p class="lead">You have successfully logged in your <strong>Library</strong> application.</p>
 
         <p><a class="btn btn-lg btn-success" href="/subject">Get started with Subjects</a></p>
     </div>
