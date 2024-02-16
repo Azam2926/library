3bdbed838a6b738b55e12428c2f5b1e4d6074241
azam (win 11) <azam.inoyatov.1998@gmail.com>
2024-02-16 22:56:05 +0500

    resolve order page

--- frontend/views/order/index.php
+++ frontend/views/order/index.php
@@ -1,102 +1,23 @@
 <?php
 
+use common\models\UserDetails;
 use frontend\dto\CartItemResponseDTO;
-use frontend\dto\UserCartDataDTO;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
-/** @var $modelData UserCartDataDTO */
-/** @var $model CartItemResponseDTO */
+/** @var $cartData CartItemResponseDTO */
+/** @var $model UserDetails */
 
-$model = $modelData->getCartItemResponseDTO();
 
-$sumOfItems = array_reduce($model->getItems(), fn($sum, $item) => $sum + ($item->getQuantity() * $item->getPrice()), 0);
+$sumOfItems = array_reduce($cartData->getItems(), fn($sum, $item) => $sum + ($item->getQuantity() * $item->getPrice()), 0);
 
 $this->title = 'Order';
 ?>
 
 <div class="container">
     <div class="row col-mb-50 gutter-50">
-        <div class="col-lg-6">
-            <h3>Billing Address</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, vel odio non dicta provident sint ex
-                autem mollitia dolorem illum repellat ipsum aliquid illo similique sapiente fugiat minus ratione.</p>
-            <form id="billing-form" name="billing-form" class="row mb-0" action="#" method="post">
-                <div class="col-md-6 form-group">
-                    <label for="billing-form-name">Name:</label>
-                    <input type="text" id="billing-form-name" name="billing-form-name" value="" class="form-control">
-                </div>
-                <div class="col-md-6 form-group">
-                    <label for="billing-form-lname">Last Name:</label>
-                    <input type="text" id="billing-form-lname" name="billing-form-lname" value="" class="form-control">
-                </div>
-                <div class="w-100"></div>
-                <div class="col-12 form-group">
-                    <label for="billing-form-companyname">Company Name:</label>
-                    <input type="text" id="billing-form-companyname" name="billing-form-companyname" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <label for="billing-form-address">Address:</label>
-                    <input type="text" id="billing-form-address" name="billing-form-address" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <input type="text" id="billing-form-address2" name="billing-form-adress" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <label for="billing-form-city">City / Town</label>
-                    <input type="text" id="billing-form-city" name="billing-form-city" value="" class="form-control">
-                </div>
-                <div class="col-md-6 form-group">
-                    <label for="billing-form-email">Email Address:</label>
-                    <input type="email" id="billing-form-email" name="billing-form-email" value="" class="form-control">
-                </div>
-                <div class="col-md-6 form-group">
-                    <label for="billing-form-phone">Phone:</label>
-                    <input type="text" id="billing-form-phone" name="billing-form-phone" value="" class="form-control">
-                </div>
-            </form>
-        </div>
-        <div class="col-lg-6">
-            <h3>Shipping Address</h3>
-            <form id="shipping-form" name="shipping-form" class="row mb-0" action="#" method="post">
-                <div class="col-md-6 form-group">
-                    <label for="shipping-form-name">Name:</label>
-                    <input type="text" id="shipping-form-name" name="shipping-form-name" value="" class="form-control">
-                </div>
-                <div class="col-md-6 form-group">
-                    <label for="shipping-form-lname">Last Name:</label>
-                    <input type="text" id="shipping-form-lname" name="shipping-form-lname" value=""
-                           class="form-control">
-                </div>
-                <div class="w-100"></div>
-                <div class="col-12 form-group">
-                    <label for="shipping-form-companyname">Company Name:</label>
-                    <input type="text" id="shipping-form-companyname" name="shipping-form-companyname" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <label for="shipping-form-address">Address:</label>
-                    <input type="text" id="shipping-form-address" name="shipping-form-address" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <input type="text" id="shipping-form-address2" name="shipping-form-adress" value=""
-                           class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <label for="shipping-form-city">City / Town</label>
-                    <input type="text" id="shipping-form-city" name="shipping-form-city" value="" class="form-control">
-                </div>
-                <div class="col-12 form-group">
-                    <label for="shipping-form-message">Notes <small>*</small></label>
-                    <textarea class="form-control" id="shipping-form-message" name="shipping-form-message" rows="6"
-                              cols="30"></textarea>
-                </div>
-            </form>
-        </div>
-        <div class="w-100"></div>
         <div class="col-lg-6">
             <h4>Your Orders</h4>
             <div class="table-responsive">
@@ -110,11 +31,12 @@ $this->title = 'Order';
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($model->getItems() as $item): ?>
+                    <?php foreach ($cartData->getItems() as $item): ?>
                         <tr class="cart_item">
                             <td class="cart-product-thumbnail">
                                 <a href="/resource/<?= $item->getUUID(); ?>">
-                                    <img width="64" height="64" src="<?= $item->getUrl() ?>" alt="<?= $item->getName() ?>">
+                                    <img width="64" height="64" src="<?= $item->getUrl() ?>"
+                                         alt="<?= $item->getName() ?>">
                                 </a>
                             </td>
                             <td class="cart-product-name">
@@ -207,9 +129,24 @@ $this->title = 'Order';
                     commodo luctus. Aenean lacinia bibendum nulla sed consectetur.
                 </div>
             </div>
-            <div class="d-flex justify-content-end">
-                <a href="#" class="button button-3d">Place Order</a>
+        </div>
+        <div class="col-lg-6 offset-lg-3">
+            <?php $form = ActiveForm::begin(); ?>
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <?= $form->field($model, 'firstname') ?>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <?= $form->field($model, 'lastname') ?>
+                </div>
             </div>
+            <?= $form->field($model, 'full_address') ?>
+            <?= $form->field($model, 'description')->textarea() ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Place Order', ['class' => 'button button-3d']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
