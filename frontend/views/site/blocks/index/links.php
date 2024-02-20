375a2764054157649dbb26b32c9643d9c50f865d
azam (win 11) <azam.inoyatov.1998@gmail.com>
2024-02-21 00:16:54 +0500

    remove instagram photo fetcher

--- frontend/views/site/blocks/index/links.php
+++ frontend/views/site/blocks/index/links.php
@@ -5,5 +5,4 @@
             <a href="https://instagram.com/book_shop" target="_blank" class="link-dark small"><u>@book_shop</u></a>
         </div>
     </div>
-    <div id="instagram-photos-carousel" class="instagram-photos customjs owl-carousel image-carousel carousel-widget mt-5" data-title="false" data-lightbox="gallery" data-count="10" data-user="blog.canvastemplate"  data-nav="true" data-pagi="false" data-items-xs="2" data-items-sm="3" data-items-lg="4" data-margin="0" data-autoplay="4000"></div>
 </div>
