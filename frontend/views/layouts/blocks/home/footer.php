d4b06f2c635f04f08fafce5d74ad1b683c1181a0
azam (win 11) <azam.inoyatov.1998@gmail.com>
2024-03-03 14:06:50 +0500

    remove footer texts

--- frontend/views/layouts/blocks/home/footer.php
+++ frontend/views/layouts/blocks/home/footer.php
@@ -1,84 +1,4 @@
 <footer id="footer" class="border-0 bg-white">
-    <div class="container">
-
-        <!-- Footer Widgets
-        ============================================= -->
-        <div class="footer-widgets-wrap">
-            <div class="row col-mb-30">
-
-                <!-- Footer Col 1 -->
-                <div class="col-lg-2 col-md-3 col-6">
-                    <div class="widget widget_links widget-li-noicon">
-                        <ul class="list-unstyled ms-0">
-                            <li class="mb-2"><a href="https://facebook.com/semicolonweb" target="_blank"> Facebook</a>
-                            </li>
-                            <li class="mb-2"><a href="https://instagram.com/semicolonweb" target="_blank"> Instagram</a>
-                            </li>
-                            <li class="mb-2"><a href="https://twitter.com/__semicolon" target="_blank"> Twitter</a></li>
-                            <li class="mb-2"><a href="https://youtube.com/c/SemiColonweb/videos" target="_blank">
-                                    YouTube</a></li>
-                        </ul>
-
-                    </div>
-                </div>
-
-                <!-- Footer Col 2 -->
-                <div class="col-lg-2 col-md-3 col-6">
-                    <div class="widget widget_links widget-li-noicon">
-                        <ul class="list-unstyled ms-0">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">FAQs</a></li>
-                            <li><a href="#">Promo</a></li>
-                            <li><a href="#">Journal</a></li>
-                        </ul>
-
-                    </div>
-                </div>
-
-                <!-- Footer Col 3 -->
-                <div class="col-md-4">
-                    <div class="widget">
-
-                        <address class="small mb-4">
-                            795 Folsom Ave, Suite 600<br>
-                            San Francisco, CA 94107<br>
-                        </address>
-
-                        <p class="mb-4 small">+ 00 11 222 4444</p>
-
-                        <p class="mb-0 text-smaller">Seamlessly disseminate covalent portals and equity invested
-                            networks. Globally evisculate holistic scenarios whereas go forward e-services. Rapidiously
-                            actualize professional data whereas impactful channels. Credibly re-engineer excellent
-                            expertise without resource-leveling e-business.</p>
-
-                    </div>
-                </div>
-
-                <!-- Footer Col 5 -->
-                <div class="col-lg-3 offset-lg-1">
-                    <div class="widget subscribe-widget" data-loader="button">
-                        <h4>20% Off on First Order</h4>
-                        <h5 class="small op-08"><strong>Subscribe</strong> to Our Newsletter to get Important News,
-                            Amazing Offers &amp; Inside Scoops:</h5>
-                        <div class="widget-subscribe-form-result"></div>
-                        <form id="widget-subscribe-form" action="include/subscribe.php" method="post" class="mb-0">
-                            <input type="email" id="widget-subscribe-form-email" name="widget-subscribe-form-email"
-                                   class="form-control required email rounded-pill"
-                                   placeholder="Enter your Email Address">
-                            <button class="button button-dark button-black rounded-pill mt-3 w-100 mx-0" type="submit">
-                                Subscribe
-                            </button>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-
-        </div><!-- .footer-widgets-wrap end -->
-
-    </div>
-
     <!-- Copyrights
     ============================================= -->
     <div id="copyrights" class="py-3 bg-white py-5">
@@ -87,17 +7,6 @@
             <div class="row justify-content-between align-items-center">
                 <div class="col-md-6">
                     Copyrights &copy; 2023 All Rights Reserved by Canvas Inc.
-                </div>
-
-                <div class="col-auto">
-                    <ul class="list-unstyled d-flex flex-row mb-2">
-                        <li class="me-2"><img src="/canvas/demos/xmas/images/cards/visa.svg" alt="Visa" width="34"></li>
-                        <li class="me-2"><img src="/canvas/demos/xmas/images/cards/mc.svg" alt="Master Card" width="34"></li>
-                        <li class="me-2"><img src="/canvas/demos/xmas/images/cards/ae.svg" alt="American Express" width="34">
-                        </li>
-                        <li class="me-2"><img src="/canvas/demos/xmas/images/cards/pp.svg" alt="PayPal" width="34"></li>
-                        <li class="me-2"><img src="/canvas/demos/xmas/images/cards/wu.svg" alt="Western Union" width="34"></li>
-                    </ul>
                 </div>
             </div>
 
